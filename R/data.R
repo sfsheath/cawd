fbe323721eec1cbbcdce5cc93ed21ca599c64d98
Sebastian Heath <sebastian.heath@gmail.com>
2015-04-12 15:31:11 -0400

    early documentation

--- R/data.R
+++ R/data.R
@@ -10,6 +10,22 @@
 #' @format SP object
 #' @source \url{http://awmc.unc.edu}
 "awmc.roman.empire.200.sp"
+#' AWMC Map of Senatorial Provinces of the Roman Empire
+#' Available from the Ancient World Mapping Center under an CC-BY-NC License 
+#'
+#' @format SP object
+#' @source \url{http://awmc.unc.edu}
+"awmc.senatorial.sp"
+#' Edited Edges from the Stanford Orbid Model of the Roman Mediterranean.
+#'
+#' @format A data frame (needs info on columns) 
+#' @source \url{http://orbis.stanford.edu}
+"orbis.edited.edges"
+#' Edited Nodes from the Stanford Orbid Model of the Roman Mediterranean.
+#'
+#' @format A data frame (needs info on columns) 
+#' @source \url{http://orbis.stanford.edu}
+"orbis.edited.nodes"
 #' Roman Amphitheaters.
 #'
 #' Data for Roman amphitheaters.
