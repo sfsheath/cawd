4a0c952c7facdca53acb7b7a47e07b45477dc293
Sebastian Heath <sebastian.heath@gmail.com>
2015-04-12 15:15:39 -0400

    first commit

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' AWMC Map of Persian Empire.
+#' Available from the Ancient World Mapping Center under an CC-BY-NC License 
+#'
+#' @format SP object
+#' @source \url{http://awmc.unc.edu}
+"awmc.persian.extent.sp"
+#' AWMC Map of Roman Empire in 200 AD.
+#' Available from the Ancient World Mapping Center under an CC-BY-NC License 
+#'
+#' @format SP object
+#' @source \url{http://awmc.unc.edu}
+"awmc.roman.empire.200.sp"
+#' Roman Amphitheaters.
+#'
+#' Data for Roman amphitheaters.
+#'
+#' @format A data frame (needs info on columns) 
+#' @source \url{http://github.com/sfsheath/roman-amphitheaters}
+"ramphs"
