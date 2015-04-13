a0e4311c4b267dd0d61249584338ad89c033f885
Sebastian Heath <sebastian.heath@gmail.com>
2015-04-12 23:51:44 -0400

    quarries redo

--- R/data.R
+++ R/data.R
@@ -30,7 +30,8 @@
 #' @source \url{http://orbis.stanford.edu}
 "orbis.edited.nodes"
 #' Oxford Roman Economy Project: Quarries.
-#'
+#' 
+#' First go at merging data from the downloable csv and from the JS map version. Probably not yet entirely correct. The data is downloadable from the OXREP website. I could not find an indication of license or restriction on use. Users may want to confer with the author (Ben Russelll) before further distribution. The original data can be cited as \'Russell, B. J. (2013). Gazetteer of Stone Quarries in the Roman World. Version 1.0. Accessed (date): oxrep.classics.ox.ac.uk/databases/stone_quarries_database/\'
 #' @format A data frame (needs info on columns) 
 #' @source \url{http://oxrep.classics.ox.ac.uk/databases/stone_quarries_database/}
 "oxrep.quarries"
